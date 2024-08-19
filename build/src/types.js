f16e52486e3c1be1076cbbbc2132d1a50ca1be93
Benjamin Ramirez <rambenrial@gmail.com>
2024-08-19 01:31:00 -0600

    Removed build folder from repo

--- build/src/types.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.LogicOperator = void 0;
-var LogicOperator;
-(function (LogicOperator) {
-    LogicOperator['And'] = 'AND';
-    LogicOperator['Or'] = 'OR';
-})((LogicOperator = exports.LogicOperator || (exports.LogicOperator = {})));
